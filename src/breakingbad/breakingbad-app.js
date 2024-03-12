3bbd9e7559ebaf77fcfbb41b66f9a947f7227045
Olaf Chwolka <olaf.chwolka@outlook.com>
2024-03-12 13:56:00 +0100

    Request http done

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -15,6 +15,24 @@ const fetchQuote = async () => {
 export const BreakingbadApp = async (element) => {
   document.querySelector("#app-title").innerHTML = "Breaking Bad App";
   element.innerHTML = "Loading...";
-  const quote = await fetchQuote();
-  element.innerHTML = "Tenemos data";
+  // await fetchQuote();
+
+  const quoteLabel = document.createElement("blockquote");
+  const authoLabel = document.createElement("h3");
+  const nextQuoteButton = document.createElement("button");
+  nextQuoteButton.innerText = "Next quote";
+
+  const renderQuote = (data) => {
+    quoteLabel.innerHTML = data.quote;
+    authoLabel.innerHTML = data.author;
+    element.replaceChildren(quoteLabel, authoLabel, nextQuoteButton);
+  };
+
+  nextQuoteButton.addEventListener("click", async () => {
+    element.innerHTML = "Loading...";
+    const quote = await fetchQuote();
+    renderQuote(quote);
+  });
+
+  fetchQuote().then(renderQuote);
 };
